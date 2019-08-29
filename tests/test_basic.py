faab6eb55c30e70678b7e38e7c09501c7e4b52f0
Nikolay Novik <nickolainovik@gmail.com>
2019-08-29 00:08:29 -0400

    Basic test cases

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,16 +1,28 @@
-import torchvision
-from torch_inspect import inspect, summary
+from torch_inspect import inspect
+from torch_inspect.inspect import LayerInfo
 
 
-def test_inspect():
-    model = torchvision.models.mobilenet_v2()
-    r = inspect(model, (3, 224, 224), device='cpu')
-    summary(model, (3, 224, 224), batch_size=100, device='cpu')
-    assert r
+def test_inspect(simple_model):
+    r = inspect(simple_model, (1, 32, 32), device='cpu')
+    L = LayerInfo
 
+    expected = [
+        L('Conv2d-1', [-1, 1, 32, 32], [-1, 6, 30, 30], True, 60),
+        L('Conv2d-2', [-1, 6, 15, 15], [-1, 16, 13, 13], True, 880),
+        L('Linear-3', [-1, 576], [-1, 120], True, 69240),
+        L('Linear-4', [-1, 120], [-1, 84], True, 10164),
+        L('Linear-5', [-1, 84], [-1, 10], True, 850),
+    ]
 
-def test_inspect2():
-    model = torchvision.models.resnet18()
-    r = inspect(model, (3, 224, 224), device='cpu')
-    summary(model, (3, 224, 224), batch_size=1, device='cpu')
-    assert r
+    assert r == expected
+
+    bsize = 10
+    r = inspect(simple_model, (1, 32, 32), bsize, device='cpu')
+    expected = [
+        L('Conv2d-1', [bsize, 1, 32, 32], [bsize, 6, 30, 30], True, 60),
+        L('Conv2d-2', [bsize, 6, 15, 15], [bsize, 16, 13, 13], True, 880),
+        L('Linear-3', [bsize, 576], [bsize, 120], True, 69240),
+        L('Linear-4', [bsize, 120], [bsize, 84], True, 10164),
+        L('Linear-5', [bsize, 84], [bsize, 10], True, 850),
+    ]
+    assert r == expected
--- torch_inspect/__init__.py
+++ torch_inspect/__init__.py
@@ -2,4 +2,3 @@ from .inspect import inspect, summary
 
 __all__ = ('inspect', 'summary')
 __version__ = '0.0.1a1'
-

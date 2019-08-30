a4f7d5cf8049efdbaec8aaa93cc5825900926e96
Nikolay Novik <nickolainovik@gmail.com>
2019-08-30 00:01:33 -0400

    Code cleanup, move dtype to separate func

--- torch_inspect/inspect.py
+++ torch_inspect/inspect.py
@@ -1,11 +1,11 @@
-import torch
 import sys
-import torch.nn as nn
 from dataclasses import dataclass
 from functools import partial
+from typing import List, IO, Union, Type
 
 import numpy as np
-from typing import List, IO
+import torch
+import torch.nn as nn
 
 
 __all__ = ('LayerInfo', 'NetworkInfo', 'inspect', 'summary')
@@ -30,7 +30,7 @@ class NetworkInfo:
     total_size: int
 
 
-def network_info(
+def make_network_info(
     info_list: List[LayerInfo], input_size: List[int], batch_size: int = -1
 ) -> NetworkInfo:
     trainable_params = 0
@@ -38,15 +38,15 @@ def network_info(
     total_output = 0
     for layer in info_list:
         total_params += layer.nb_params
-        total_output += np.prod(layer.output_shape)
+        total_output += abs(np.prod(layer.output_shape))
         if layer.trainable:
             trainable_params += layer.nb_params
 
     # assume 4 bytes/number (float on cuda).
-    total_input_size = abs(np.prod(input_size) * batch_size * 4 / (1024 ** 2))
+    total_input_size = np.prod(input_size) * abs(batch_size) * 4
     # x2 for gradients
-    total_output_size = abs(2 * total_output * 4 / (1024 ** 2))
-    total_params_size = abs(total_params * 4 / (1024 ** 2))
+    total_output_size = 2 * total_output * 4
+    total_params_size = total_params * 4
     total_size = total_params_size + total_output_size + total_input_size
     ni = NetworkInfo(
         total_params,
@@ -68,13 +68,30 @@ def should_attach_hook(model: nn.Module, module: nn.Module) -> bool:
     return v
 
 
+_FTensorType = Type[Union[torch.cuda.FloatTensor, torch.FloatTensor]]
+
+
+def _infer_dtype(device: str) -> _FTensorType:
+    device = device.lower()
+    if device not in ('cuda', 'cpu'):
+        msg = 'Input device is not valid, please specify "cuda" or "cpu"'
+        raise ValueError(msg)
+
+    if device == 'cuda' and torch.cuda.is_available():
+        dtype = torch.cuda.FloatTensor
+    else:
+        dtype = torch.FloatTensor
+    return dtype
+
+
 def inspect(
     model: nn.Module,
     input_size: List[int],
     batch_size: int = -1,
     device: str = 'cpu',
 ) -> List[LayerInfo]:
-    def register_hook(module):
+    def register_hook(module: nn.Module) -> None:
+
         def hook(module, input, output):
             class_name = str(module.__class__).split('.')[-1].split("'")[0]
             module_idx = len(layer_list)
@@ -92,9 +109,9 @@ def inspect(
             params = 0
             trainable = False
             if hasattr(module, 'weight') and hasattr(module.weight, 'size'):
-                params += torch.prod(
+                params += int(torch.prod(
                     torch.LongTensor(list(module.weight.size()))
-                )
+                ))
                 trainable = module.weight.requires_grad
 
             if hasattr(module, 'bias') and hasattr(module.bias, 'size'):
@@ -102,24 +119,15 @@ def inspect(
                     torch.LongTensor(list(module.bias.size()))
                 )
 
-            l = LayerInfo(
+            layer = LayerInfo(
                 name, input_shape, output_shape, trainable, int(params)
             )
-            layer_list.append(l)
+            layer_list.append(layer)
 
         if should_attach_hook(model, module):
             hooks.append(module.register_forward_hook(hook))
 
-    device = device.lower()
-    if device not in ('cuda', 'cpu'):
-        msg = 'Input device is not valid, please specify "cuda" or "cpu"'
-        raise ValueError(msg)
-
-    if device == 'cuda' and torch.cuda.is_available():
-        dtype = torch.cuda.FloatTensor
-    else:
-        dtype = torch.FloatTensor
-
+    dtype = _infer_dtype(device)
     # multiple inputs to the network
     if isinstance(input_size, tuple):
         input_size = [input_size]
@@ -151,7 +159,7 @@ def summary(
     flush: bool = False,
 ) -> None:
     summary = inspect(model, input_size, batch_size=batch_size, device=device)
-    n = network_info(summary, input_size, batch_size)
+    n = make_network_info(summary, input_size, batch_size)
     print_ = partial(print, file=file, flush=flush)
     print_('\n')
     print_('----------------------------------------------------------------')
@@ -168,14 +176,14 @@ def summary(
             '{0:,}'.format(layer.nb_params),
         )
         print_(line_new)
-
+    MB = 1024 ** 2
     print_('================================================================')
     print_(f'Total params: {n.total_params:,}')
     print_(f'Trainable params: {n.trainable_params:,}')
     print_(f'Non-trainable params: {n.total_params - n.trainable_params:,}')
     print_('----------------------------------------------------------------')
-    print_(f'Input size (MB): {n.total_input_size:0.2f}')
-    print_(f'Forward/backward pass size (MB): {n.total_output_size:0.2f}')
-    print_(f'Params size (MB): {n.total_params_size:0.2f}')
-    print_(f'Estimated Total Size (MB): {n.total_size:0.2f}')
+    print_(f'Input size (MB): {n.total_input_size / MB:0.2f}')
+    print_(f'Forward/backward pass size (MB): {n.total_output_size / MB:0.2f}')
+    print_(f'Params size (MB): {n.total_params_size / MB :0.2f}')
+    print_(f'Estimated Total Size (MB): {n.total_size / MB:0.2f}')
     print_('----------------------------------------------------------------')

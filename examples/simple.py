14b19825815b41451358a3a6fc3171196f48ac37
Nikolay Novik <nickolainovik@gmail.com>
2019-08-30 22:47:53 -0400

    Add simple usage example

---
+++ examples/simple.py
@@ -0,0 +1,38 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch_inspect as ti
+
+
+class SimpleNet(nn.Module):
+    def __init__(self):
+        super(SimpleNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 3)
+        self.conv2 = nn.Conv2d(6, 16, 3)
+        self.fc1 = nn.Linear(16 * 6 * 6, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+
+def main():
+    net = SimpleNet()
+    ti.summary(net, (1, 32, 32), device='cpu')
+
+
+if __name__ == '__main__':
+    main()
